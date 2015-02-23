0fb32d266738296d2571494f77594623d013380c
Katye Russell <katye.russell@gmail.com>
2015-02-23 15:21:00 -0600

    First commit.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade'], dest: 'public/', filter: 'isFile'}
+        ]
+      }
+    },
+    jade: {
+      compile: {
+        files: [{expand: true, cwd: 'app/', src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}]
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('build', ['copy', 'jade']);
+};
